b3b479b275280977572c3667ba9727d475193ba9
isaacwrk <isaacsouzawrk@gmail.com>
2025-12-08 23:48:24 -0300

    fix url fe

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3333'
+      apiBase: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:3333'
     }
   },
 
