ef55b5ede6e1672f3121e0a8829dd8c2a363a115
isaacwrk <isaacsouzawrk@gmail.com>
2025-12-08 23:54:36 -0300

    test variabel

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:3333'
+      apiBase: process.env.PUBLIC_API_URL || 'http://localhost:3333'
     }
   },
 
